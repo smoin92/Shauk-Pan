9f12012a511b0ecb51f4704a0fc54948976bf6ff
smoin92 <khawaja.shk98@gmail.com>
2026-05-11 15:47:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ let currentPaymentType = ""; // HALF or FULL
 /************************************
  * API ENDPOINTS
  ************************************/
-const INVENTORY_API = "https://moin31.app.n8n.cloud/webhook/pan-inventory";
+const INVENTORY_API = "https://uheman5.app.n8n.cloud/webhook/pan-inventory";
 const ORDER_API = "https://moin31.app.n8n.cloud/webhook/paan-order";
-const PAYMENT_API = "https://moin31.app.n8n.cloud/webhook/payment";
+const PAYMENT_API = "https://uheman5.app.n8n.cloud/webhook/paan-order";
 
 /************************************
  * DOM ELEMENT REFERENCES
